79105fa97eb74c3d4592930f143711c9d29e9615
shrutipistolwala-tech <shruti.pistolwala@tum.de>
2021-08-05 11:49:35 +0200

    second commit

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//  Addition
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+// End
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+//  Addition
+import 'hammerjs';
+// End
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //  Addition
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule
+    // End
   ],
   providers: [],
   bootstrap: [AppComponent]
